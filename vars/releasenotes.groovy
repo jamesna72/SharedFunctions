0a1a4e514175c5b7b0f0ab3bbd022fd44379fa35
jamesna72 <68188922+jamesna72@users.noreply.github.com>
2025-01-02 12:59:37 +1300

    Update releasenotes.groovy

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -1,19 +1,19 @@
-import groovy.io.FileType;
-import java.io.File;
+import groovy.io.*;
+import java.io.*;
 
 
-def dir = new File(pwd());
+def dir = new File("C:\\Azure");
     
-new File(dir.path + '/releasenotes.txt').withWriter('utf-8') 
+new File(dir.path + '\\releasenotes.txt').withWriter('utf-8') 
 { 
 	writer -> 
-	dir.eachFileRecurse(FileType.ANY){ file ->
-	if (file.isDirectory()){
+	    dir.eachFileRecurse(FileType.ANY){ file ->
+	    if (file.isDirectory()){
 		   writer.writeLine(file.name);            
-	}
-	else
-	{
+	    }
+	    else
+	    {
 		writer.writeLine('\t' + file.name + '\t' + file.length());
-	}
-		 
+	    }
+	} 
 }
